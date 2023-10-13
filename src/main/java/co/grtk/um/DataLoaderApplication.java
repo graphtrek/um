cfe78779c699cc95665ffd4022818465889135ad
Imre <imre.tatai@gmail.com>
2023-10-14 00:33:38 +0200

    fixes

--- src/main/java/co/grtk/um/DataLoaderApplication.java
+++ src/main/java/co/grtk/um/DataLoaderApplication.java
@@ -9,19 +9,16 @@ import org.dhatim.fastexcel.reader.ReadableWorkbook;
 import org.dhatim.fastexcel.reader.Row;
 import org.dhatim.fastexcel.reader.Sheet;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.*;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -39,12 +36,11 @@ public class DataLoaderApplication implements CommandLineRunner {
     }
 
 
-    public void loadData() {
-        log.info("DataLoaderApplication starting ...");
+    public void loadData() throws Exception {
+        File xlsx = new ClassPathResource("/lt1.xlsx").getFile();
+        log.info("DataLoaderApplication starting xlsx:{}",xlsx);
 
-        Map<Integer, List<String>> data = new HashMap<>();
-
-        try (FileInputStream file = new FileInputStream("/Users/Imre/IdeaProjects/grtk/um/src/main/resources/lt1.xlsx"); ReadableWorkbook wb = new ReadableWorkbook(file)) {
+        try (FileInputStream file = new FileInputStream(xlsx); ReadableWorkbook wb = new ReadableWorkbook(file)) {
             Sheet sheet = wb.getFirstSheet();
             try (Stream<Row> rows = sheet.openStream()) {
                 rows.forEach(r -> {
@@ -84,24 +80,9 @@ public class DataLoaderApplication implements CommandLineRunner {
                             .orElse("");
                     userActivityLogDTO.setTextParams(textParam);
                     userActivityLogDTO.setAppId("GEORGE");
-
-                    data.put(r.getRowNum(), new ArrayList<>());
-
-                    for (Cell cell : r) {
-                        if(Objects.isNull(cell))
-                            data.get(r.getRowNum()).add("");
-                        else
-                            data.get(r.getRowNum()).add(cell.getRawValue());
-                    }
-
-                    //log.info("{}", userActivityLogDTO);
                     kafkaPublisherService.logUserActivity(userActivityLogDTO);
                 });
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
